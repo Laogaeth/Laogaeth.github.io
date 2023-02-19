413ef3dc29e9315b41d5823191575812c9a7f22d
Pedro <pedro3dpereira@gmail.com>
2023-02-19 09:05:41 +0000

    Clean up

--- main.js
+++ main.js
@@ -1,33 +1,4 @@
 
-        //star animations
-
-        function randomNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min
-        }
-        
-        const STAR_COUNT = 400
-        let result = ""
-        for(let i = 0; i < STAR_COUNT; i++){
-            result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
-        }
-        console.log(result.substring(0, result.length - 1))
-        
-    
-
-
-        //star animations
-
-        function randomNumber(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min
-        }
-        
-        const STAR_COUNT = 400
-        let result = ""
-        for(let i = 0; i < STAR_COUNT; i++){
-            result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
-        }
-        console.log(result.substring(0, result.length - 1))
-        
     
 
         //star animations
